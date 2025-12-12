6e3dea13d2495d5790702a3b267ea1e5a71ce50f
woneung-lee <85263184+woneung-lee@users.noreply.github.com>
2025-12-12 09:45:53 +0900

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Firebase 설정 - 본인의 Firebase 프로젝트 설정으로 교체하세요
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAXpp13ARkgrGMHBvccZI5WMz5S3FlC9gw",
+  authDomain: "lunchbox-d547e.firebaseapp.com",
+  projectId: "lunchbox-d547e",
+  storageBucket: "lunchbox-d547e.firebasestorage.app",
+  messagingSenderId: "726812509012",
+  appId: "1:726812509012:web:ec734f89aa3a9f8c15bec1"
 };
 
 // Firebase 초기화
